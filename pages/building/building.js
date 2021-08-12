0c0ec590dae84a196624763b18edc0de50896180
chenzihong-gavin <191250014@smail.nju.edu.cn>
2021-08-13 00:37:29 +0800

    merge

--- pages/building/building.js
+++ pages/building/building.js
@@ -17,7 +17,7 @@ Page({
     var that= this;
     // 加载页面数据（封面等
     that.setData({
-      picture:"../../static/image/index/封面.png",
+      picture:"../../static/image/index/cover.png",
       building_name:"炜华体育场",
       intro_text:"伟华体育场是仙林校区最活跃的体育运动场，位于教学楼旁侧，是举办大型校内活动的专属地点"
     })
